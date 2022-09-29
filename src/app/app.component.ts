11b0a89ba9d237ab7974c53b500bed0b02efbe6b
vsarabudla2020 <venkatreddys2000@gmail.com>
2022-09-29 19:25:52 -0400

    finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MenuItem } from './menu-item';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Myapp';
+  title = 'Chirpus Pizza!';
+
+MenuItem= [{
+              Item:"Chiken Fingers",
+              Category:"dinner",
+              Price:11.99},
+              {
+                Item:"Pizza",
+                Category:"dinner",
+                Price:11.99},
+              {
+                Item:"Wings",
+                Category:"sides",
+                Price:8.99},
+              {
+                Item:"Breadsticks",
+                Category:"sides",
+                Price:4.99},
+              {
+                Item:"Cesar Salad",
+                Category:"salads",
+                Price:5.99},
+              {
+                Item:"Cinnamon Roll",
+                Category:"dessert",
+                Price:8.99},
+  
+            ]
+ 
+
 }
+ 
+
+
